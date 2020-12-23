8ec207b2b79d9c2bd2f1c5b9475ed565149f9ad4
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2020-12-23 11:55:11 +0000

    [LeetCode Sync] Runtime - 1 ms (98.65%), Memory - 36.9 MB (100.00%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,38 +1,20 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            Character curr = s.charAt(i);
-            if(isOpenParenthese(curr)) {
-                stack.push(curr);
+    public boolean isValid(String str) {
+       Stack<Character> charStack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(' || c == '{' || c == '[') { 
+                charStack.push(c);
             } else {
-                
-                if(stack.isEmpty()) {
+                if (!charStack.isEmpty() && (charStack.peek() == c - 1 || charStack.peek() == c - 2)) {
+                    charStack.pop();
+                } else
                     return false;
-                } else if (!checkValidParenthese(stack.peek(), curr)) {
-                    return false;
-                } else {
-                    stack.pop();                    
-                }
-                
             }
         }
-
-        return stack.isEmpty();
-    }
-               
-    public boolean checkValidParenthese(char open, char close) {
-        if(open == '(' && close == ')') return true;
-        if(open == '[' && close == ']') return true;
-        if(open == '{' && close == '}') return true;
-        
-        return false;
-    }
-    
-    public boolean isOpenParenthese(char ch) {
-        return ch == '(' || ch == '{' || ch == '[';
+        if (charStack.isEmpty())
+            return true;
+        else
+            return false;
     }
 }
