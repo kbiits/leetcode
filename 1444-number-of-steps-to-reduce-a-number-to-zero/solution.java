af9f6c5819621b68a14ccad93f91ef0af90490eb
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2022-03-05 08:31:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.7 MB (7.07%)

--- 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -1,15 +1,7 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int step = 0;
-        while(num > 0) {
-            if(num % 2 == 0) {
-                num = num / 2;
-            } else {
-                num--;
-            }
-            step++;
-        }
-        
-        return step;
+        if (num == 0)
+            return 0;
+        return 1 + numberOfSteps(num % 2 == 0 ? num / 2 : num - 1);
     }
 }
