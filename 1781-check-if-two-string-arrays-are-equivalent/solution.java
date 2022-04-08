e8998d0e2490190f7d06e3678f88e429974ac1de
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2022-04-08 06:15:54 +0000

    [LeetCode Sync] Runtime - 2 ms (22.89%), Memory - 41.7 MB (5.03%)

--- 1781-check-if-two-string-arrays-are-equivalent/solution.java
+++ 1781-check-if-two-string-arrays-are-equivalent/solution.java
@@ -1,14 +1,32 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
+        int pointer1Inner, pointer2Inner, pointer1Outer, pointer2Outer;
+        pointer1Inner = pointer1Outer = pointer2Inner = pointer2Outer = 0;
 
-        for (String word : word1) {
-            s1.append(word);
+        while (pointer1Outer < word1.length && pointer2Outer < word2.length) {
+            String curr1 = word1[pointer1Outer];
+            String curr2 = word2[pointer2Outer];
+            int n1 = curr1.length();
+            int n2 = curr2.length();
+            while (pointer1Inner < n1 && pointer2Inner < n2) {
+                if (curr1.charAt(pointer1Inner) != curr2.charAt(pointer2Inner)) {
+                    return false;
+                }
+
+                pointer1Inner++;
+                pointer2Inner++;
+            }
+
+            if (pointer1Inner >= n1) {
+                pointer1Outer++;
+                pointer1Inner = 0;
+            }
+            if (pointer2Inner >= n2) {
+                pointer2Outer++;
+                pointer2Inner = 0;
+            }
         }
-        for (String word : word2) {
-            s2.append(word);
-        }
-        return s1.toString().equals(s2.toString());
+
+        return pointer1Outer == word1.length && pointer2Outer == word2.length;
     }
 }
