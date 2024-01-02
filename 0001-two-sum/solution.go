7d1ec01e334d82168715b2c7caf95ebb517c7fd2
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-01-02 17:56:35 +0000

    [LeetCode Sync] Runtime - 13 ms (37.93%), Memory - 4.3 MB (18.50%)

--- 0001-two-sum/solution.go
+++ 0001-two-sum/solution.go
@@ -1,17 +1,13 @@
 func twoSum(nums []int, target int) []int {
-	var table = map[int]int{}
-	var result = []int{0, 0}
-	for i := 0; i < len(nums); i++ {
-		curr := nums[i]
-		complement := target - curr
-		if v, ex := table[complement]; ex {
-			result[0] = v
-            result[1] = i
-            break
-		}
+    var mapSum = map[int]int{}
+    for i, num := range nums {
+        complement := target - num
+        if v, ex := mapSum[complement]; ex {
+            return []int{i, v}
+        } else {
+            mapSum[num] = i
+        }
+    }
 
-		table[curr] = i
-	}
-
-	return result
+    return []int{-1, -1}
 }
