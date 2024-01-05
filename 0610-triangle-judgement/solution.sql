0a19ec48e3a13cfec1f9aa42b416e71b70aa3f62
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-01-05 16:58:09 +0000

    [LeetCode Sync] Runtime - 463 ms (69.20%), Memory - 0B (100.00%)

---
+++ 0610-triangle-judgement/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    t.*, 
+    CASE 
+        WHEN t.x + t.y > t.z AND t.y + t.z > t.x AND t.z + t.x > t.y THEN 'Yes'    
+        ELSE 'No'
+    END as triangle
+FROM Triangle t;
