ca695210b9746624681fc6d2ac144f489da21503
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-01-13 11:44:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.7 MB (45.79%)

---
+++ 1603-running-sum-of-1d-array/solution.go
@@ -0,0 +1,12 @@
+func runningSum(nums []int) []int {
+    result := make([]int, len(nums))
+    for i, v := range nums {
+        if i == 0 {
+            result[i] = v
+        } else {
+            result[i] = result[i - 1] + v
+        }
+    }
+
+    return result
+}
