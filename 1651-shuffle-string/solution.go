722d7224f223a844493e61d151cf7f75e019c85b
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-01-13 16:34:02 +0000

    [LeetCode Sync] Runtime - 6 ms (50.00%), Memory - 3.2 MB (50.00%)

---
+++ 1651-shuffle-string/solution.go
@@ -0,0 +1,10 @@
+func restoreString(s string, indices []int) string {
+    val := make([]rune, len(s))
+
+    for i := 0; i < len(s); i++ {
+        index := indices[i]
+        val[index] = rune(s[i])
+    }
+
+    return string(val)
+}
