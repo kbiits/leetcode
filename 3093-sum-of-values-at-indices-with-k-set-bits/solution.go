bc374319121e1ca3a107be1c53f3f1a9706387dd
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-01-13 12:33:17 +0000

    [LeetCode Sync] Runtime - 3 ms (96.94%), Memory - 4.1 MB (46.94%)

--- 3093-sum-of-values-at-indices-with-k-set-bits/solution.go
+++ 3093-sum-of-values-at-indices-with-k-set-bits/solution.go
@@ -1,8 +1,44 @@
+// func sumIndicesWithKSetBits(nums []int, k int) int {
+//     sum := 0
+//     for i, v := range nums {
+//         // count1s := count1sInBinary(i)
+
+//         // Brian Kernighan’s Algorithm for counting 1's bit (set bit)
+//         // in binary representation of a number
+//         index := i
+//         count1s := 0
+//         for index > 0 {
+//             count1s++
+//             index = index & (index - 1)
+//         }
+
+//         if count1s == k {
+//             sum += v
+//         }
+//     }
+
+//     return sum
+// }
+
+var mapBits = make([]int, 256)
+func populateLookupTableBits() {
+    mapBits[0] = 0
+    for i := 0; i < 256; i++ {
+        mapBits[i] = (i & 1) + mapBits[i / 2]
+    }
+}
+
 func sumIndicesWithKSetBits(nums []int, k int) int {
+    populateLookupTableBits()
+
     sum := 0
     for i, v := range nums {
-        count1s := count1sInBinary(i)
-        if count1s == k {
+        n := i
+        countOf1s := mapBits[n & 0xff] + 
+            mapBits[(n >> 8) & 0xff] + 
+            mapBits[(n >> 16) & 0xff] + 
+            mapBits[n >> 24]
+        if countOf1s == k {
             sum += v
         }
     }
@@ -10,12 +46,3 @@ func sumIndicesWithKSetBits(nums []int, k int) int {
     return sum
 }
 
-func count1sInBinary(n int) int {
-    count := 0
-    for n > 0 {
-        count++
-        n = n & (n - 1)
-    }
-
-    return count
-}
