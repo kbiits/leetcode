e6c03c09320a745d5856515e744b429e23b3f028
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-03-17 07:52:41 +0000

    [LeetCode Sync] Runtime - 14 ms (93.44%), Memory - 6.1 MB (81.15%)

---
+++ 2042-maximum-product-difference-between-two-pairs/solution.go
@@ -0,0 +1,26 @@
+
+func maxProductDifference(nums []int) int {
+    firstMax := -1
+    secondMax := -2
+
+    firstMin := 10002
+    secondMin := 10001
+
+    for _, v := range nums {
+        if v > firstMax {
+            secondMax = firstMax
+            firstMax = v
+        } else if v > secondMax {
+            secondMax = v
+        }
+
+        if v < firstMin {
+            secondMin = firstMin
+            firstMin = v
+        } else if v < secondMin {
+            secondMin = v
+        }
+    }
+
+    return (firstMax * secondMax) - (firstMin * secondMin)
+}
