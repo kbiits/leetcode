f2ed97f0e51979d4dad8ec07442a13bbe951f540
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-04-30 17:05:31 +0000

    [LeetCode Sync] Runtime - 4 ms (63.79%), Memory - 3.2 MB (31.05%)

---
+++ 0083-remove-duplicates-from-sorted-list/solution.go
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    for node := head; node != nil; node = node.Next {
+        for node.Next != nil && node.Val == node.Next.Val {
+            node.Next = node.Next.Next
+        }
+    }
+
+    return head
+}
