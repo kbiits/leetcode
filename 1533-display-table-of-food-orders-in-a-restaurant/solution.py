2d204428296f053c6eab42da1760e272503ae323
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-06-03 19:22:27 +0000

    [LeetCode Sync] Runtime - 314 ms (82.32%), Memory - 27.1 MB (95.45%)

---
+++ 1533-display-table-of-food-orders-in-a-restaurant/solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        header = ["Table"]
+        map_table = {}
+        item_set = set()
+
+        for order in orders:
+            item = order[2]
+            
+            item_set.add(item)
+
+            table_no = order[1]
+            if not (table_no in map_table):
+                map_table[table_no] = {}
+
+            if item in map_table[table_no]:
+                map_table[table_no][item] += 1
+            else:
+                map_table[table_no][item] = 1
+    
+        result = [header]
+        sorted_item = sorted(item_set)
+
+        for item in sorted_item:
+            header.append(item)
+
+        for k, v in sorted(map_table.items(), key=lambda item: int(item[0])):
+            row = [k]
+            for item in sorted_item:
+                row.append(str(v[item] if item in v else "0"))
+            result.append(row)
+
+        return result
