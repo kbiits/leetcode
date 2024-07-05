14479c5d46c167515b1308ee7e727601e925544e
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2024-07-05 19:24:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.6 MB (67.92%)

--- 0021-merge-two-sorted-lists/solution.go
+++ 0021-merge-two-sorted-lists/solution.go
@@ -6,22 +6,16 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    newHead := &ListNode{}
-    ptr := newHead
-
-
-    for list1 != nil || list2 != nil {
-        if (list1 != nil && list2 != nil && list1.Val <= list2.Val) || list2 == nil {
-            ptr.Next = list1
-            list1 = list1.Next
-        } else if list2 != nil {
-            ptr.Next = list2
-            list2 = list2.Next
-        }
-
-        ptr = ptr.Next
+    if list1 == nil {
+        return list2
+    } else if list2 == nil {
+        return list1
     }
 
-    return newHead.Next
+    if list1.Val < list2.Val {
+        list1.Next = mergeTwoLists(list2, list1.Next)
+        return list1
+    }
+    list2.Next = mergeTwoLists(list1, list2.Next)
+    return list2
 }
-
