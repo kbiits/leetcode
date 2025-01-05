f23bcd03bd4dc79eec3268a84487acfa7bb0b4e8
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2025-01-05 15:37:19 +0000

    [LeetCode Sync] Runtime - 4 ms (29.03%), Memory - 18.9 MB (34.65%)

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,30 +1,9 @@
 class Solution:
-
-    # old approach
-    # we don't need set because the array is sorted
-    # def removeDuplicates(self, nums: List[int]) -> int:
-    #     theset = set()
-    #     # count = 0
-    #     fast_ptr = 0
-    #     slow_ptr = 0
-    #     while fast_ptr < len(nums):
-    #         curr = nums[fast_ptr]
-    #         if curr not in theset:
-    #             theset.add(curr)
-    #             nums[slow_ptr] = nums[fast_ptr]
-    #             fast_ptr += 1
-    #             slow_ptr += 1
-    #         else:
-    #             fast_ptr += 1
-
-    #     return slow_ptr
-
     def removeDuplicates(self, nums: List[int]) -> int:
-        ptr_replace = 1
-        for checking_ptr in range(1, len(nums)):
-            if nums[checking_ptr] != nums[checking_ptr - 1]:
-                nums[ptr_replace] = nums[checking_ptr]
-                ptr_replace += 1
-
-        return ptr_replace
+        ptr = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[ptr] = nums[i]
+                ptr += 1
+        return ptr
                 
