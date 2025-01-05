a0aec33ce533ce08d0da9210a163d7abe41eb4cc
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2025-01-05 16:06:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (23.49%)

--- 0027-remove-element/solution.py
+++ 0027-remove-element/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        ptr_replace = 0
-        for ptr_check in range(len(nums)):
-            if nums[ptr_check] != val:
-                nums[ptr_replace] = nums[ptr_check]
-                ptr_replace += 1
-
-        return ptr_replace
+        ptr_write = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[ptr_write] = nums[i]
+                ptr_write += 1
+        return ptr_write
